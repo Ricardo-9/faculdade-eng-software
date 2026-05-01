e61c0e613f1b1e4d9a8d2d188a3e2a124240aa80
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-01 20:37:52 -0300

    feat: adiciona o exercício 2, uma função para cálculo de média simples entre 3 notas

--- introducao-programacao/src/aula01-variaveis/ex01-soma.ts
+++ introducao-programacao/src/aula01-variaveis/ex01-soma.ts
@@ -4,4 +4,4 @@ function somar(a: number, b: number) {
   return a + b;
 }
 
-console.log(`A soma é : ${somar(2, 5)}`);
+console.log(`A soma é : ${somar(2, 8)}`);
---
+++ introducao-programacao/src/aula01-variaveis/ex02-média.ts
@@ -0,0 +1,11 @@
+//Exercício 2 - Média de três notas
+
+function calcularMedia(nota1: number, nota2: number, nota3: number) {
+  if (nota1 < 0 || nota2 < 0 || nota3 < 0) {
+    throw new Error("O valor de uma nota não pode ser negativo");
+  }
+
+  return (nota1 + nota2 + nota3) / 3;
+}
+
+console.log(`A média é : ${calcularMedia(2, 3, -4)}`);
