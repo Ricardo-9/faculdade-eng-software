dd6aa9daba166f5a5237975b801024fa5158e493
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-01 20:59:27 -0300

    feat: adiciona o exercício 3, uma função para cálculo aproximado da idade de uma pessoa com base no ano atual

---
+++ introducao-programacao/src/aula01-variaveis/ex03-idade.ts
@@ -0,0 +1,18 @@
+//Exercício 3 - Cáculo aproximado da idade do usuário com base no ano de nascimento
+
+function calcularIdade(dataDeNascimento: number, anoAtual?: number) {
+  const dataAtual = anoAtual ?? new Date().getFullYear();
+
+  if (!Number.isInteger(dataDeNascimento) || !Number.isInteger(dataAtual)) {
+    throw new Error("As datas devem ser valores inteiros");
+  }
+  if (dataDeNascimento > dataAtual) {
+    throw new Error("O ano de nascimento não pode ser um valor futuro");
+  }
+
+  return dataAtual - dataDeNascimento;
+}
+
+//O parãmetro de ano atual é opcional, caso não seja definido manualmente pelo usuário, será calculado internamente pela função
+console.log(`A idade é : ${calcularIdade(2008, 2026)}`);
+console.log(`A idade é : ${calcularIdade(2008)}`);
