85c92280b4c48d45451eccc953f5f91d1ca709b7
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-02 12:14:15 -0300

    feat: adiciona o exercício 17, uma função que calcula a média entre 3 idades

---
+++ introducao-programacao/src/aula01-variaveis/ex17-media-idades.ts
@@ -0,0 +1,20 @@
+//Exercício 17 - Calcula a média entre 3 idades
+
+function mediaDeIdades(idade1: number, idade2: number, idade3: number): number {
+  if (idade1 < 0 || idade2 < 0 || idade3 < 0) {
+    throw new Error("As idades devem ser valores não negativos");
+  }
+  if (
+    !Number.isInteger(idade1) ||
+    !Number.isInteger(idade2) ||
+    !Number.isInteger(idade3)
+  ) {
+    throw new Error("As idades devem ser números inteiros");
+  }
+
+  return (idade1 + idade2 + idade3) / 3;
+}
+
+const mediaCalculada = mediaDeIdades(23, 45, 34);
+
+console.log(`A média de idades calculada é : ${mediaCalculada}`);
