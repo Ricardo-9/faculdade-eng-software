38de59b6a5b1ff3e0ed465727536b45a3d0d87bd
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-01 21:06:20 -0300

    feat: adiciona o exercício 4, uma função para conversão de medidas em metros para centímetros

---
+++ introducao-programacao/src/aula01-variaveis/ex04-m-para-cm.ts
@@ -0,0 +1,11 @@
+//Exercício 4 - Conversão de metros para centímetros
+
+function metroParaCentimetro(medidaEmMetros: number) {
+  if (medidaEmMetros < 0) {
+    throw new Error("A medida em metros não pode ser negativa");
+  }
+
+  return medidaEmMetros * 100;
+}
+
+console.log(`Valor em centímetros : ${metroParaCentimetro(26)}`);
