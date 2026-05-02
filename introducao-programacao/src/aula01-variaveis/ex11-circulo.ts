0d608de2e777177b8ffd580caf4a7a43e2758751
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-02 10:21:57 -0300

    feat: adiciona o exercício 11, uma função que calcula e retorna a área de um círculo

---
+++ introducao-programacao/src/aula01-variaveis/ex11-circulo.ts
@@ -0,0 +1,11 @@
+//Exercício 11 - Calcula a área de um círculo
+
+function areaCirculo(raio: number): number {
+  if (raio < 0) {
+    throw new Error("O raio de um cículo não pode assumir valores negativos");
+  }
+
+  return (3.14 * raio) ^ 2;
+}
+
+console.log(`A área do círculo é : ${areaCirculo(6)}`);
