4bc4164c40aa9a7cfabdbc34f0003cf35e4b780d
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-06 10:30:37 -0300

    refactor: melhorado lógica do exercício 7, troca de variáveis

--- introducao-programacao/src/aula01-variaveis/ex07-troca.ts
+++ introducao-programacao/src/aula01-variaveis/ex07-troca.ts
@@ -1,9 +1,12 @@
 //Exercício 7, uma função que realize a troca do valor de duas variáveis
 
 export default function trocaDeValor(a: number, b: number): [number, number] {
-  return [b, a];
+  const temp = a;
+  a = b;
+  b = temp;
+  return [a, b];
 }
 
 // const [novoA, novoB] = trocaDeValor(1, 7);
 
-// console.log(`A: ${novoA}, B: ${novoB}`);
+// console.log(`A: ${novoA},  B: ${novoB}`);
