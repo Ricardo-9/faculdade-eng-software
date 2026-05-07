36c4eac94ea4fbe0394f18186eaf2f7bdd67114b
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:26:29 -0300

    feat: adiciona o exercício 09 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex09-idade.py
@@ -0,0 +1,6 @@
+#Exercício  - calcule a idade de uma pessoa
+
+anoAtual = int(input("Escreva o ano atual: "))
+anoNascimento = int(input("Escreva o ano de nascimento: "))
+
+print("A idade da pessoa é:",anoAtual - anoNascimento)
