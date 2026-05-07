f528abdfa785f099379bd258a79dfde9836da24f
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:00:17 -0300

    feat: adiciona o exercício 02 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex02-perfil.py
@@ -0,0 +1,7 @@
+#Exercício 2 - Crie um programa que exiba três informações
+
+nome = input("Escreva o seu nome: ")
+idade = int(input("Escreva sua idade: "))
+cidade = input("Escreva o nome da cidade onde mora: ")
+
+print(" Seu nome é:",nome,"\n Sua idade é:",idade,"\n Você mora em:",cidade) 
