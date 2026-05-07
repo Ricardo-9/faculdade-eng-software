fc972f4318acc2683923d0e41a41528f174a5ef5
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:36:27 -0300

    feat: adiciona o exercício 16 e 17 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex16-desconto.py
@@ -0,0 +1,5 @@
+#Exercício 16 - calcule o valor de um produto com desconto de 10%
+
+preco = float(input("insira o preço do produto: "))
+
+print("O preço do produto com desconto é:",preco * 0.9)
---
+++ introducao-programacao/src/aula02-python/ex17-aumento.py
@@ -0,0 +1,5 @@
+#Exercício 17 - calcule o valor de um produto com aumento de 15%
+
+preco = float(input("insira o preço do produto: "))
+
+print("O preço do produto com aumento é:",preco * 1.15)
