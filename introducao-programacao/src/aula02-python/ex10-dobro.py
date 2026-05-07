ffce967c3a1fabc53d23ef45bbd3b97df73a07ed
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:28:49 -0300

    feat: adiciona o exercício 10 e 11 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex10-dobro.py
@@ -0,0 +1,5 @@
+#Exercício 10 - calcule o dobro de um número
+
+numero = float(input("insira um número: "))
+
+print("O dobro do número é:",numero * 2)
---
+++ introducao-programacao/src/aula02-python/ex11-triplo.py
@@ -0,0 +1,5 @@
+#Exercício 13 - calcule o triplo de um número
+
+numero = float(input("insira um número: "))
+
+print("O triplo do número é:",numero * 3)
