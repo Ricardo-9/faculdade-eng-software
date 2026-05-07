60903d8e49cc9e7815a700c04bbac62ce2490f1e
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:16:10 -0300

    feat: adiciona o exercício 05 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex05-soma.py
@@ -0,0 +1,6 @@
+#Exercício 5 - some dois valores
+
+numero1 = float(input("insira o valor do primeiro número: "))
+numero2 = float(input("insira o valor do segundo número: "))
+
+print("A soma é:",numero1 + numero2)
