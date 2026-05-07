b8a36c6961c83f441fa933f9682426780c98a3f7
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:18:45 -0300

    feat: adiciona o exercício 06 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex06-subtracao.py
@@ -0,0 +1,6 @@
+#Exercício 6 - subtraia dois valores
+
+numero1 = float(input("insira o valor do primeiro número: "))
+numero2 = float(input("insira o valor do segundo número: "))
+
+print("A diferença é:",numero1 - numero2)
