637d16b7bf5a917d522f4117e38979213178b679
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:34:28 -0300

    feat: adiciona o exercício 14 e 15 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex14-km-m.py
@@ -0,0 +1,5 @@
+#Exercício 14 - converta uma distância de km para m
+
+km = float(input("insira a distância em km: "))
+
+print("A distância em metros é:",km * 1000)
---
+++ introducao-programacao/src/aula02-python/ex15-media.py
@@ -0,0 +1,7 @@
+#Exercício 15 - calcule a média de 3 notas
+
+nota1 = float(input("insira a primeira nota: "))
+nota2 = float(input("insira a segunda nota: "))
+nota3 = float(input("insira a terceira nota: "))
+
+print("A média das notas é:",(nota1 + nota2 + nota3) / 3)
