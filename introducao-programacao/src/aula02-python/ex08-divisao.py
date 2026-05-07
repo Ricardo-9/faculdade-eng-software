0998b4fe860e08e72267eb192b946821dc5cfa32
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:21:33 -0300

    feat: adiciona o exercício 08 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex08-divisao.py
@@ -0,0 +1,6 @@
+#Exercício 8 - divida dois valores
+
+numero1 = float(input("insira o valor do primeiro número: "))
+numero2 = float(input("insira o valor do segundo número: "))
+
+print("A divisão é:",numero1 / numero2)
