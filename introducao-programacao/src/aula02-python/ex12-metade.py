99dc51f97b6a0c72e6658f4700e17635d66b4640
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:31:43 -0300

    feat: adiciona o exercício 12 e 13 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex12-metade.py
@@ -0,0 +1,5 @@
+#Exercício 12 - calcula a metade de um número
+
+numero = float(input("insira um número: "))
+
+print("A metade do número é:",numero / 2)
---
+++ introducao-programacao/src/aula02-python/ex13-m-cm.py
@@ -0,0 +1,5 @@
+#Exercício 13 - converta uma medida de metros para centímetros
+
+metros = float(input("insira a medida em metros: "))
+
+print("A medida em centímetros é:",metros * 100)
