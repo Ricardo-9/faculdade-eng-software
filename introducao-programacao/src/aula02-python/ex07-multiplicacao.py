d9ff2e122fa36b324a3d3e22c9ab7d6d0f6a8d5a
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-05-07 09:20:15 -0300

    feat: adiciona o exercício 07 da lista 02 sobre python

---
+++ introducao-programacao/src/aula02-python/ex07-multiplicacao.py
@@ -0,0 +1,6 @@
+#Exercício 7 - multiplique dois valores
+
+numero1 = float(input("insira o valor do primeiro número: "))
+numero2 = float(input("insira o valor do segundo número: "))
+
+print("O produto é:",numero1 * numero2)
